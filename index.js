5a2eeb8987a3f85dbf057eadde8c3799d9b4b219
Martyna <martyna.glaz@gmail.com>
2019-05-04 22:58:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 // Write your solution in this file!
-const driver = [function drivers(driver, key, value){
+const driver = [function drivers(){
   console.log = driver;
 }]
-function updateDriverWithKeyAndValue(driver, name, address){
-return{
-  driver.name = "Sam";
-  driver.address = "11 Broadway"};
-}
-}
