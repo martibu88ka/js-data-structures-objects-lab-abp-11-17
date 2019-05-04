14850c8b59356f1478afecdddfc06e52f80f9fe1
Martyna <martyna.glaz@gmail.com>
2019-05-04 23:37:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your solution in this file!
 function drivers(name, address){
-  let driver=drivers(name,address)
   return{
     name:name,
     address:address,
   };
 }
+let driver=drivers(name,address)
