ab970c19880c027c63628b3245a01ecfe1ee2582
Martyna <martyna.glaz@gmail.com>
2019-05-04 23:02:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@
 let driver = [function drivers(){
   console.log = driver;
 }]
+function updateDriverWithKeyAndValue(driver, 'address', '11 Broadway'){}
+===
+function updateDriverWithKeyAndValue(driver, 'name', 'Sam'){}
