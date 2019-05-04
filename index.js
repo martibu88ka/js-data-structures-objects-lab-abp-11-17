d5bb502704c1ed35fecc9eec18c34755138d3c74
Martyna <martyna.glaz@gmail.com>
2019-05-04 23:53:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // Write your solution in this file!
 let driver = {name:"Sam", address:"11 Broadway"}
+function updateDriverWithKeyAndValue(driver, name, "Sam"){
+  let newdriver = {...driver};
+
+  newdriver[name]="Sam";
+
+  return newdriver.name;
+}
